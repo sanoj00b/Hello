0f2262ba60ddf88b6c2eedcea8ae8bf150dcb08d
sanoj <sanoj@bldm3295-mac.corp.bluejeans.com>
2018-12-26 14:01:46 +0530

    Initial commit

---
+++ Hello/PrintHello.swift
@@ -0,0 +1,17 @@
+//
+//  PrintHello.swift
+//  Hello
+//
+//  Created by Sanoj  Kashya on 24/12/18.
+//  Copyright © 2018 Sanoj  Kashyap. All rights reserved.
+//
+
+import Foundation
+
+
+open class PrintClass {
+    
+    public func printHello(){
+        print("Hello Dear")
+    }
+}
